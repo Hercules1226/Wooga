c073368e8ac18886ae55bc384ddcf1d70151fad8
injaepak <85613541+injaepak@users.noreply.github.com>
2021-11-30 15:43:43 +0900

    arrow

--- Wooga/Source/Wooga/Private/Arrow.cpp
+++ Wooga/Source/Wooga/Private/Arrow.cpp
@@ -2,29 +2,67 @@
 
 
 #include "Arrow.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "VR_Player.h"
+#include <Kismet/GameplayStatics.h>
 
 // Sets default values
 AArrow::AArrow()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	rootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(rootComp);
+
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Component"));
+	meshComp->SetupAttachment(rootComp);
 }
 
 // Called when the game starts or when spawned
 void AArrow::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	player = Cast<AVR_Player>(UGameplayStatics::GetActorOfClass(GetWorld(), AVR_Player::StaticClass()));
 }
 
 // Called every frame
 void AArrow::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	currentTime += DeltaTime;
+	if (currentTime >= 2.f)
+	{
+		meshComp->SetHiddenInGame(false);
+	}
 
-	GetActorUpVector();
+	if (player->camLoc)
+	{
+		if (meshComp->bHiddenInGame == false)
+		{
+			rotDir = player->camLookAt->GetComponentLocation() - GetActorLocation();
+			rotDir.Normalize();
+			targetrot = rotDir.ToOrientationRotator();
+			curRot = GetActorRotation();
+			targetRotYaw = targetrot.Yaw;
+			curRot = FMath::Lerp(curRot, FRotator(90.f, targetRotYaw, 0.f), 1 * DeltaTime);
 
-	SetActorLocation(GetActorLocation() + dir * moveSpeed * GetWorld()->DeltaTimeSeconds);
+			SetActorRotation(curRot);
+		}
+	}
+
+	/*if(currentTime <= 0.5f)
+	{
+	dir = meshComp->GetForwardVector() * pm;
+
+	SetActorLocation(meshComp->GetComponentLocation() + dir * moveSpeed * GetWorld()->DeltaTimeSeconds /5);
+	}
+	if (currentTime > 0.5f)
+	{
+		pm = pm * (-1.f);
+		currentTime = 0.f;
+	}*/
 }
 
---
+++ Wooga/Source/Wooga/Public/Arrow4.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Arrow4.generated.h"
+
+UCLASS()
+class WOOGA_API AArrow4 : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AArrow4();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setting)
+		class USceneComponent* rootComp;
+
+	UPROPERTY(EditAnywhere, Category = Setting)
+		class UStaticMeshComponent* meshComp;
+
+	UPROPERTY()
+		float pm = 1;
+
+	UPROPERTY()
+		FVector dir;
+
+	UPROPERTY()
+		float currentTime;
+
+	UPROPERTY()
+		float moveSpeed = 50.f;
+
+	// È¸Àü
+
+	UPROPERTY()
+		class AVR_Player* player;
+
+	UPROPERTY()
+		FVector rotDir;
+
+	UPROPERTY()
+		FRotator targetrot;
+
+	UPROPERTY()
+		FRotator curRot;
+
+	UPROPERTY()
+		float targetRotYaw;
+
+	UPROPERTY()
+		bool arrowOn = false;
+};
